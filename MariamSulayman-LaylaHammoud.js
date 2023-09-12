2e86249816a111baf8c0e6b1c58ff1607688c572
Layla Hammoud <laylahammoud63@gmail.com>
2023-09-12 14:59:07 +0300

    finish the fist exercice

---
+++ MariamSulayman-LaylaHammoud.js
@@ -0,0 +1,11 @@
+var keys = 0;
+switch(keys) { // switch case to check the number of keys collected so it will display the appropriate message 
+  case 0:
+    console.log("we sank together")
+    break;
+  case 30000:
+    console.log("we made it")
+    break;
+  default:
+    console.log("we need to swim a little bit more!")
+}
