689a82a633235964e565dbb7fbeb6dfce484d4c6
Layla Hammoud <laylahammoud63@gmail.com>
2023-09-12 15:09:04 +0300

    finish the second exercice

--- MariamSulayman-LaylaHammoud.js
+++ MariamSulayman-LaylaHammoud.js
@@ -1,11 +1,24 @@
-var keys = 0;
-switch(keys) { // switch case to check the number of keys collected so it will display the appropriate message 
-  case 0:
-    console.log("we sank together")
-    break;
-  case 30000:
-    console.log("we made it")
-    break;
-  default:
-    console.log("we need to swim a little bit more!")
+// var keys = 0;
+// switch(keys) { // switch case to check the number of keys collected so it will display the appropriate message 
+//   case 0:
+//     console.log("we sank together")
+//     break;
+//   case 30000:
+//     console.log("we made it")
+//     break;
+//   default:
+//     console.log("we need to swim a little bit more!")
+// }
+
+var arr = [5, 10, 20, 1, 15, -1];
+var min = arr[0];
+var max = arr[0];
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i]<min){
+        min = arr[i]
+    }
+    if (arr[i]>max){
+        max=arr[i]
+    }
 }
+console.log(`${min} and ${max}`)
